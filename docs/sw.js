991425725bca4bd919e6c30a1cf1918078813694
acontreras15-portátil <acontreras15@ucol.mx>
2025-08-26 10:08:19 -0600

    Prueba mil

--- docs/sw.js
+++ docs/sw.js
@@ -1,29 +1,14 @@
-const CACHE_NAME = "mensajes-cache-v1";
+const CACHE_NAME = "mi-cache-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/sw.js"
+  "./",
+  "./index.html",
+  "./manifest.json",
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
     })
   );
 });
